4abd81d266accba7ed26c710ad4bf4f616b8f321
Anirudh S <anirudh@Anirudhs-MacBook-Air.local>
2022-05-11 03:43:49 -0700

    setup

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,335 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import { Navbar } from "react-bootstrap";
+import AddIdeaForm from "./AddIdeaForm";
+import OpenIdeaForm from "./OpenIdeaForm";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
+import axios from "axios";
+import API from "../../backend";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import AppBarmenu from "./../AppBarmenu";
+import Grid from "@mui/material/Grid";
+import "./Home.css";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Label,
+} from "recharts";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "left",
+  color: theme.palette.text.secondary,
+}));
+
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFC20F",
+  "#F39200",
+  "#F35C6E",
+  "#5B61AA",
+];
+
+const Home = (props) => {
+  // const renderCustomLabel = (entry) => {
+  //   return `${entry.status} ${entry.count}`;
+  // };
+  const [totalCount, setCount] = useState(0);
+  const [ideas, setIdeas] = useState([]);
+  const [hours, setHours] = useState(0);
+  const [ideasInformation, setIdeasInformation] = useState([]);
+  const [ideaCount, setIdeaCount] = useState([]);
+  const [ideaStatus, setIdeaStatus] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openIdea, setOpenIdea] = useState(false);
+  const [index, setIndex] = useState(-1);
+  const [votes, setVotes] = useState(0);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleOpenIdea = (id) => {
+    setOpenIdea(true);
+    console.log("success", id);
+    setIndex(id);
+    setVotes(ideas[id].votes);
+  };
+  const handleCloseIdea = () => setOpenIdea(false);
+  const style = {
+    position: "absolute",
+    top: "30%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 800,
+    bgcolor: " #fff",
+    border: "2px solid #fff",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  React.useEffect(() => {
+    axios.get(API + "/dashboard/getIdeas").then((response) => {
+      setIdeas(response.data.ideas);
+      setIdeaStatus(response.data.ideaStatus);
+      setCount(response.data.totalCount);
+      if (response.data.ideaCount !== null) {
+        setIdeaCount(response.data.ideaCount);
+      }
+    });
+    console.log("IDEALIST", ideas);
+
+    axios.get(API + "/dashboard/getHoursForIdea").then((response) => {
+      setHours(response.data.hours);
+    });
+    console.log("HOURS", hours);
+
+    axios.get(API + "/dashboard/getIdeasInformation").then((response) => {
+      setIdeasInformation(response.data.ideasInformation);
+      setIdeaStatus(response.data.ideaStatus);
+    });
+    console.log("IDEAINFOLIST", JSON.stringify(ideasInformation));
+    console.log(ideaStatus);
+  }, []);
+
+  return (
+    <>
+      <AppBarmenu />
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="home">
+              <h2
+                style={{
+                  "font-weight": "bold",
+                  "font-family": "Roboto",
+                  color: "#0173ce",
+                }}
+              >
+                &nbsp;&nbsp;&nbsp; Ideas Overview
+              </h2>
+            </Navbar.Brand>
+            <Button variant="contained" onClick={handleOpen}>
+              Add Idea
+            </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <h3>Create Idea</h3>
+                <br />
+                <AddIdeaForm />
+              </Box>
+            </Modal>
+            <Modal
+              open={openIdea}
+              onClose={handleCloseIdea}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <h3>Idea Information</h3>
+                <br />
+                <OpenIdeaForm
+                  infoIdea={ideasInformation}
+                  ideaIndex={index}
+                  ideaVotes={votes}
+                />
+              </Box>
+            </Modal>
+          </Navbar>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>
+            <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Status</h4>
+            <br />
+            <Grid container spacing={4}>
+              <Grid item xs={5}>
+                <ResponsiveContainer width="100%" height="99%" aspect={2}>
+                  <PieChart>
+                    <Pie
+                      data={ideaStatus}
+                      dataKey="count"
+                      outerRadius={90}
+                      innerRadius={60}
+                      startAngle={90}
+                      endAngle={-270}
+                      isAnimationActive={true}
+                      label
+                      nameKey="status"
+                    >
+                      {ideaStatus.map((entry, index) => (
+                        <Cell key={`cell-${entry}`} fill={COLORS[index]} />
+                      ))}
+                      <Label
+                        value={`${totalCount} Ideas`}
+                        position="center"
+                        fill="grey"
+                        style={{
+                          fontSize: "28px",
+                          fontWeight: "bold",
+                          fontFamily: "Roboto",
+                        }}
+                      />
+                    </Pie>
+                    <Tooltip />
+                  </PieChart>
+                </ResponsiveContainer>
+              </Grid>
+              <Grid item xs={7}>
+                <Card sx={{ height: "70%", width: "80%" }}>
+                  <CardContent>
+                    <Grid container spacing={2}>
+                      <Grid item xs={6}>
+                        <div class="input-color">
+                          <div
+                            class="color-box"
+                            style={{ "background-color": "#0088FE" }}
+                          ></div>
+                          &nbsp;&nbsp;&nbsp;&nbsp; Already Exists
+                        </div>
+                        <hr />
+                        <div class="input-color">
+                          <div
+                            class="color-box"
+                            style={{ "background-color": "#00C49F" }}
+                          ></div>
+                          &nbsp;&nbsp;&nbsp;&nbsp; Will Not Implement
+                        </div>
+                        <hr />
+                        <div class="input-color">
+                          <div
+                            class="color-box"
+                            style={{ "background-color": "#FFC20F" }}
+                          ></div>
+                          &nbsp;&nbsp;&nbsp;&nbsp; Future Consideration
+                        </div>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <div class="input-color">
+                          <div
+                            class="color-box"
+                            style={{ "background-color": "#F39200" }}
+                          ></div>
+                          &nbsp;&nbsp;&nbsp;&nbsp; Planned
+                        </div>
+                        <hr />
+                        <div class="input-color">
+                          <div
+                            class="color-box"
+                            style={{ "background-color": "#F35C6E" }}
+                          ></div>
+                          &nbsp;&nbsp;&nbsp;&nbsp; Shipped
+                        </div>
+                        <hr />
+                        <div class="input-color">
+                          <div
+                            class="color-box"
+                            style={{ "background-color": "#5B61AA" }}
+                          ></div>
+                          &nbsp;&nbsp;&nbsp;&nbsp; Needs Review
+                        </div>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Item>
+          <Item>
+            <hr />
+            <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ideas</h4>
+            <br />
+            <ResponsiveContainer height="100%" width="88%" aspect={2.4}>
+              <BarChart
+                width={500}
+                height={300}
+                data={ideaCount}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend layout="vertical" verticalAlign="top" align="right" />
+                <Bar dataKey="count" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
+          </Item>
+        </Grid>
+        <Grid item xs={4} style={{ justifyContent: "space-between" }}>
+          <Item style={{ height: "100%" }}>
+            <Grid>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    color="text.secondary"
+                  >
+                    Ideas Portal
+                  </Typography>
+
+                  <Typography variant="body2" color="text.secondary">
+                    <br />
+                    CMPE-272 Ideas Portal
+                  </Typography>
+                </CardContent>
+                {/* <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions> */}
+              </Card>
+            </Grid>
+            <hr />
+            <p>
+              <strong>&nbsp;&nbsp;New Ideas</strong>
+            </p>
+            {ideas.map((item, i) => (
+              <li
+                style={{ fontSize: "12px" }}
+                onClick={() => handleOpenIdea(i)}
+              >
+                PROD-I-{i} <a>{item.name}</a>{" "}
+                {hours !== 0 && (
+                  <span style={{ color: "blue" }}> {item.hours} Hours </span>
+                )}
+                <label className={"right"}>{item.date}</label>
+              </li>
+            ))}
+            <hr />
+            <p>
+              <strong>&nbsp;&nbsp;Popular Ideas</strong>
+            </p>
+            <hr />
+            <p>
+              <strong>&nbsp;&nbsp;Top Contributors</strong>
+            </p>
+          </Item>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Home;
---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 3001;
+const cors = require("cors");
+
+// changed the URL for AWS deployment
+// If anyone pulls this code change it to frontendURL = "http://localhost:3000"
+// Once you are done and want to push DON'T FORGET to change it back to frontendURL = "http://35.89.120.177:3000"
+
+// const frontendURL = "http://localhost:3000";
+const frontendURL = "http://35.89.120.177:3000";
+
+var session = require("express-session");
+
+app.use(cors({ origin: frontendURL, credentials: true }));
+app.use(bodyParser.json());
+
+//Allow Access Control
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", frontendURL);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Cache-Control", "no-cache");
+  next();
+});
+
+app.use(
+  session({
+    secret: "cmpe273_kafka_passport_mongo",
+    resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
+    saveUninitialized: false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
+    duration: 60 * 60 * 1000, // Overall duration of Session : 30 minutes : 1800 seconds
+    activeDuration: 5 * 60 * 1000,
+  })
+);
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routing Logic
+const dashboardRoute = require("./Routes/dashboard");
+
+//Routes
+app.use("/dashboard", dashboardRoute);
+
+module.exports = app;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
